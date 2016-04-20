cc4c080b970feb0b69e125616ab3a4ced95f587b
Nick Peto <nick.petosa@gmail.com>
2016-04-19 20:44:41 -0400

    utils

---
+++ Kumquat .NET/Utils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRecommender
+{
+    public class Utils
+    {
+        public static List<String> getTitles(String q, String aspect)
+        {
+            List<String> arr = new List<String>();
+            while(q.Contains("\""+ aspect +"\":"))
+            {
+                int index = q.IndexOf("\"" + aspect + "\":");
+                index += 4 + aspect.Length;
+                String build = "";
+                char at = q[index];
+                while(q[index] != '"')
+                {
+                    build += q[index];
+                }
+                arr.Add(build);
+            }
+            return arr;
+        }
+
+    }
+}
