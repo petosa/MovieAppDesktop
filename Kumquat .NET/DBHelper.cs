0be62a0a9a1309eacb48182960e8180ffbaa4f6a
William Ma <willzma15@gmail.com>
2016-04-26 20:48:21 -0400

    sheet

--- Kumquat .NET/DBHelper.cs
+++ Kumquat .NET/DBHelper.cs
@@ -1,13 +1,26 @@
-﻿using System;
+﻿using Kumquat.NET.model;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Kumquat.NET
-{
-    class DBHelper
-    {
+namespace Kumquat.NET {
+    static class DBHelper {
 
+        private static User currentUser = null;
+        private static Dictionary<String, User> allUsers = new Dictionary<String, User>();
+        private static Dictionary<String, Movie> allMovies = new Dictionary<String, Movie>();
+
+        public static User getCurrentUser() { return currentUser; }
+        public static Dictionary<String, User> getUsersMap() { return allUsers; }
+        public static Dictionary<String, Movie> getMoviesMap() { return allMovies; }
+
+        public static void setCurrentUser(User u) { currentUser = u; }
+
+        public static String getDigest(String password) {
+
+        }
     }
 }
